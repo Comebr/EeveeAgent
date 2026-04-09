f3fd9e69376a4e6bbe60fe165024f3115888fcdc
最后一张黑桃Ace <wzy1621178619@126.com>
2026-04-09 16:11:29 +0800

    手动管理LLM

--- eevee-ai/src/main/java/com/azheng/agent/config/LLMConfig.java
+++ eevee-ai/src/main/java/com/azheng/agent/config/LLMConfig.java
@@ -1,15 +1,41 @@
 package com.azheng.agent.config;
 
-import dev.langchain4j.community.dashscope.spring.DashScopeChatModelProperties;
-import dev.langchain4j.community.model.dashscope.QwenChatModel;
-import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * 其他职责大模型配置
- */
 @Configuration
 public class LLMConfig {
 
+    /**
+     * 流式输出模型-百炼
+     */
+    @Bean(name = "qwen-flash")
+    public StreamingChatModel QwenFlashStreamingChatModel(){
+        return OpenAiStreamingChatModel
+                .builder()
+                .apiKey(System.getenv("BAILIAN_API_KEY"))
+                .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
+                .modelName("qwen-flash")
+                .build();
+    }
+
+
+
+
+
+    /**
+     * 向量模型-百炼
+     */
+    @Bean(name = "embedding-v4")
+    public EmbeddingModel defaultEmbeddingModel(){
+        return OpenAiEmbeddingModel
+                .builder()
+                .modelName("text-embedding-v4")
+                .dimensions(4096)
+                .build();
+    }
 }
