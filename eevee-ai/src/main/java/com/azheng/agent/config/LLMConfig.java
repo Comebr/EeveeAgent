316aad6aede9dd083080fb7d390351af36e84202
最后一张黑桃Ace <wzy1621178619@126.com>
2026-04-10 18:36:53 +0800

    读取配置文件设置

--- eevee-ai/src/main/java/com/azheng/agent/config/LLMConfig.java
+++ eevee-ai/src/main/java/com/azheng/agent/config/LLMConfig.java
@@ -4,21 +4,32 @@ import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LLMConfig {
 
+    @Value(value = "${langchain4j.open-ai.base-url}")
+    private String baseUrl;
+
+    @Value(value = "${langchain4j.open-ai.api-key}")
+    private String apiKey;
+
+    @Value(value = "${langchain4j.open-ai.embedding-model.dimensions}")
+    private Integer dimensions;
+
     /**
      * 流式输出模型-百炼
+     * 该模型最大向量维度为 3072
      */
     @Bean(name = "qwen-flash")
     public StreamingChatModel QwenFlashStreamingChatModel(){
         return OpenAiStreamingChatModel
                 .builder()
-                .apiKey(System.getenv("BAILIAN_API_KEY"))
-                .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
+                .apiKey(apiKey)
+                .baseUrl(baseUrl)
                 .modelName("qwen-flash")
                 .build();
     }
@@ -34,8 +45,10 @@ public class LLMConfig {
     public EmbeddingModel defaultEmbeddingModel(){
         return OpenAiEmbeddingModel
                 .builder()
+                .apiKey(apiKey)
+                .baseUrl(baseUrl)
                 .modelName("text-embedding-v4")
-                .dimensions(4096)
+                .dimensions(dimensions)
                 .build();
     }
 }
