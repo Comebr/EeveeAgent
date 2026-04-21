e50ef991d759dcad1c9978d272b94c4c247a21ac
最后一张黑桃Ace <wzy1621178619@126.com>
2026-04-21 15:26:21 +0800

    修改标题请求类

---
+++ main-application/src/main/java/com/azheng/boot/rag/controller/request/MkdirTitleRequest.java
@@ -0,0 +1,11 @@
+package com.azheng.boot.rag.controller.request;
+
+import lombok.Data;
+
+@Data
+public class MkdirTitleRequest {
+
+    private String conversationId;
+
+    private String title;
+}
