ccb4cc107c8e99e36235080562fce62c9f844313
最后一张黑桃Ace <wzy1621178619@126.com>
2026-04-24 15:38:12 +0800

    添加rag下的Mapper扫描路径

--- main-application/src/main/java/com/azheng/boot/EeveeApplication.java
+++ main-application/src/main/java/com/azheng/boot/EeveeApplication.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan(basePackages ={
         "com.azheng.boot.user.mapper",
-        "com.azheng.boot.kb.dao.mapper"})
+        "com.azheng.boot.kb.dao.mapper",
+        "com.azheng.boot.rag.dao.mapper"})
 @ComponentScan(basePackages = {"com.azheng.boot", "com.azheng.agent", "com.azheng.framework"})
 public class EeveeApplication {
     public static void main(String[] args) {SpringApplication.run(EeveeApplication.class, args);}
