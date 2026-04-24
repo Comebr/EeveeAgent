86f3a166034e0ee3678bda6f74625e55d6275af5
最后一张黑桃Ace <wzy1621178619@126.com>
2026-04-24 15:54:08 +0800

    删除

--- main-application/src/main/java/com/azheng/boot/user/config/MybatisPlusConfig.java
+++
@@ -1,9 +0,0 @@
-package com.azheng.boot.user.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan({"com.azheng.boot.user.mapper", "com.azheng.boot.intent.dao.mapper"})
-public class MybatisPlusConfig {
-}
