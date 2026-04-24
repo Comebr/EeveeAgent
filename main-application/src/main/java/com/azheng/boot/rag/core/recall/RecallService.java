b860149ee8b29a5919b481935e44675a57014a18
最后一张黑桃Ace <wzy1621178619@126.com>
2026-04-24 15:51:36 +0800

    多路召回业务类适配多路检索引擎

--- main-application/src/main/java/com/azheng/boot/rag/core/recall/RecallService.java
+++ main-application/src/main/java/com/azheng/boot/rag/core/recall/RecallService.java
@@ -1,126 +1,108 @@
 package com.azheng.boot.rag.core.recall;
 
 import cn.hutool.core.collection.CollUtil;
-import com.azheng.boot.rag.core.embedding.milvus.config.MilvusEmbeddingStoreConfig;
 import com.azheng.boot.rag.core.embedding.milvus.operation.MilvusOperations;
 import com.azheng.boot.rag.core.embedding.milvus.operation.MilvusRetrievalResult;
 import com.azheng.boot.rag.core.intent.IntentNode;
 import com.azheng.boot.rag.core.intent.NodeScore;
 import com.azheng.boot.rag.core.intent.SubQuestionIntent;
-import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.rag.content.Content;
-import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-import dev.langchain4j.rag.query.Query;
-import dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore;
 import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
-import static com.azheng.boot.rag.constant.RAGConstant.INTENT_MIN_SCORE;
 
 /**
  * lc4j-recall-多路召回文本Chunk
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class RecallService {
-
-    @Resource
-    private MilvusEmbeddingStoreConfig milvusEmbeddingStoreConfig;
 
     @Resource
     private MilvusOperations milvusOperations;
 
-    @Resource
-    private EmbeddingModel embeddingModel;
+    @Qualifier("ragInnerRetrievalThreadPoolExecutor")
+    private final Executor recallChunksExecutor;
 
-    @Value("${recall.top-n}")
-    private Integer TopN;
+
     /**
-     * retrieval-for-lc4j-ContentAggregator
-     * 对一个子问题进行召回
-     *
+     * 意图定向并行检索
+     */
+    public List<List<MilvusRetrievalResult>> recallMilvusContentWithKB(SubQuestionIntent subQuestionIntent, Integer TopK) {
+        // 这里能进来的意图绝对是KB的
+
+        // 粗筛：多路召回Chunk
+        // 1.无意图，返回空
+        if (CollUtil.isEmpty(subQuestionIntent.nodeScores())) {
+            return List.of();
+        }
+        // 子问题
+        String subQuestion = subQuestionIntent.subQuestion();
+
+        // 子问题的KB意图
+        List<NodeScore> nodeScores = subQuestionIntent.nodeScores();
+
+        List<CompletableFuture<List<MilvusRetrievalResult>>> futures = nodeScores.stream()
+                .map(
+                        nodeScore -> {
+                            CompletableFuture<List<MilvusRetrievalResult>> future = CompletableFuture.supplyAsync(
+                                    () -> {
+                                        // 1.获取节点信息
+                                        IntentNode node = nodeScore.getNode();
+                                        String collectionName = node.getCollectionName();
+
+                                        // 2.检索出当前意图下的Chunk
+                                        List<MilvusRetrievalResult> milvusRetrievalResults = milvusOperations.search(subQuestion, collectionName,TopK );
+                                        return milvusRetrievalResults;
+                                    }, recallChunksExecutor
+                            );
+                            return future;
+                        }
+                ).toList();
+
+        List<List<MilvusRetrievalResult>> chunksList = futures.stream()
+                .map(CompletableFuture::join)
+                .toList();
+
+        return CollUtil.isEmpty(chunksList) ? List.of() : chunksList;
+    }
+
+    /**
+     * 向量全局并行检索
+     * @param subQuestion
+     * @param collections
+     * @param TopK
      * @return
      */
-    public Map<String, List<MilvusRetrievalResult>> recallMilvusContentWithKB(SubQuestionIntent subQuestionIntent) {
-        // 这里能进来的意图绝对是KB的
-        // 当前子问题召回的Chunks总列表
-        Map<String,List<MilvusRetrievalResult>> reCallContextMap = new HashMap<>();
-
-        // 粗筛：多路召回Chunk
-            // 1.无意图，返回空
-            if(CollUtil.isEmpty(subQuestionIntent.nodeScores())){
-                return Map.of();
-            }
-            // 子问题
-            String subQuestion = subQuestionIntent.subQuestion();
-
-            // 子问题的KB意图
-            List<NodeScore> nodeScores = subQuestionIntent.nodeScores();
-
-            // 召回所有意图（一个子问题可能存在多个KB意图）
-            nodeScores.stream(). forEach(nodeScore -> {
-
-                // 1.获取节点信息
-                IntentNode node = nodeScore.getNode();
-                String collectionName = node.getCollectionName();
-                String nodeId = node.getId();
-
-                // 2.检索出当前意图下的Chunk
-                List<MilvusRetrievalResult> milvusRetrievalResults = milvusOperations.search(subQuestion, collectionName);
-
-                // 3.追加本次node的召回结果
-                reCallContextMap.put(nodeId, milvusRetrievalResults);
-            });
-        // 4.返回检索总结果
-        return reCallContextMap;
-    }
-
-    public Collection<List<Content>> recallMilvusContentInLc4j(String question,List<SubQuestionIntent> subQuestionIntentList) {
-        //总Chunks列表
-        Collection<List<Content>> contentsList = new ArrayList<>();
-
-        // 粗筛：多路召回Chunk
-        for (SubQuestionIntent subQuestionIntent : subQuestionIntentList) {
-            // 1.无意图结果，下一个
-            if(CollUtil.isEmpty(subQuestionIntent.nodeScores())){
-                continue;
-            }
-            String subQuestion = subQuestionIntent.subQuestion();
-            List<NodeScore> nodeScores = subQuestionIntent.nodeScores();
-            nodeScores.stream(). forEach(nodeScore -> {
-                IntentNode node = nodeScore.getNode();
-                String collectionName = node.getCollectionName();
-                String nodeId = node.getId();
-                // 2.锁定集合Collection
-                MilvusEmbeddingStore milvusEmbeddingStore = milvusEmbeddingStoreConfig.creteMilvusEmbeddingStore(collectionName);
-
-                // 3.构建向量检索器
-                EmbeddingStoreContentRetriever embeddingStoreContentRetriever = EmbeddingStoreContentRetriever
-                        .builder()
-                        .minScore(INTENT_MIN_SCORE) //最低分数阈值，会自动过滤
-                        .maxResults(TopN) //TopN 单次最大检索Content数量
-                        .embeddingModel(embeddingModel) //嵌入模型
-                        .embeddingStore(milvusEmbeddingStore) //嵌入存储
-                        .build();
-                /**
-                 * !!! Content中的metadata还包含对Chunk的评分score以及id !!!
-                 * 这是做rerank的关键！
-                 */
-                // 4.检索出当前意图下的Chunk
-
-                Query query = Query.from(subQuestion);
-                List<Content> contents = embeddingStoreContentRetriever.retrieve(query);
-
-                //5.追加本次进总召回列表
-                contentsList.add(contents);
-            });
+    public List<List<MilvusRetrievalResult>> recallMilvusContentWithGlobal(String subQuestion,List<String> collections, Integer TopK) {
+        if (CollUtil.isEmpty(collections)) {
+            return List.of();
         }
-        // 6.返回检索总结果
-        return contentsList;
+
+        List<CompletableFuture<List<MilvusRetrievalResult>>> futures = collections.stream()
+                .map(collection -> {
+                    CompletableFuture<List<MilvusRetrievalResult>> future = CompletableFuture.supplyAsync(
+                            () -> {
+                                List<MilvusRetrievalResult> milvusRetrievalResults = milvusOperations.search(subQuestion, collection, TopK);
+                                return milvusRetrievalResults;
+
+                            }, recallChunksExecutor
+                    );
+                    return future;
+                }).toList();
+
+        List<List<MilvusRetrievalResult>> chunksList = futures.stream()
+                .map(CompletableFuture::join)
+                .toList();
+        return CollUtil.isEmpty(chunksList) ? List.of() : chunksList;
     }
+
 }
