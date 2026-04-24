85461d8e900919acb2677ad6940e58c8a04732c7
最后一张黑桃Ace <wzy1621178619@126.com>
2026-04-24 15:57:06 +0800

    新增向量检索方法

--- main-application/src/main/java/com/azheng/boot/rag/core/embedding/milvus/operation/MilvusOperations.java
+++ main-application/src/main/java/com/azheng/boot/rag/core/embedding/milvus/operation/MilvusOperations.java
@@ -1,7 +1,6 @@
 package com.azheng.boot.rag.core.embedding.milvus.operation;
 
 import cn.hutool.core.lang.Assert;
-import com.azheng.boot.rag.core.embedding.milvus.config.MilvusClientConfig;
 import com.azheng.framework.exception.ClientException;
 import com.azheng.framework.exception.ServiceException;
 import com.google.gson.Gson;
@@ -9,7 +8,9 @@ import com.google.gson.JsonObject;
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.output.Response;
 import io.milvus.v2.client.MilvusClientV2;
+import io.milvus.v2.common.ConsistencyLevel;
 import io.milvus.v2.common.DataType;
 import io.milvus.v2.common.IndexParam;
 import io.milvus.v2.service.collection.request.AddFieldReq;
@@ -17,15 +18,17 @@ import io.milvus.v2.service.collection.request.CreateCollectionReq;
 import io.milvus.v2.service.collection.request.GetLoadStateReq;
 import io.milvus.v2.service.database.request.CreateDatabaseReq;
 import io.milvus.v2.service.vector.request.DeleteReq;
+import io.milvus.v2.service.vector.request.SearchReq;
 import io.milvus.v2.service.vector.request.UpsertReq;
+import io.milvus.v2.service.vector.request.data.BaseVector;
+import io.milvus.v2.service.vector.request.data.FloatVec;
+import io.milvus.v2.service.vector.response.SearchResp;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Milvus通用操作
@@ -72,7 +75,7 @@ public class MilvusOperations {
 
     /**
      * 创建collection(该方法仅适用于存储知识块表)
-     * @param collectionName
+     * @param collectionName 集合名
      */
     public boolean createChunkCollection(String collectionName) {
         Assert.notNull(collectionName,()->new ServiceException("Milvus<UNK>"));
@@ -105,6 +108,13 @@ public class MilvusOperations {
                         .fieldName("chunk_text_varchar")
                         .dataType(DataType.VarChar)
                         .maxLength(dimension)
+                        .build());
+
+        schema.addField(AddFieldReq
+                        .builder()
+                        .fieldName("metadata")
+                        .dataType(DataType.JSON)
+                        .isNullable(true)
                         .build());
 
         // 3.配置索引
@@ -200,6 +210,69 @@ public class MilvusOperations {
             log.error("Collection名称已存在");
             throw new ClientException("Collection名称已存在");
         }
+    }
+
+    /**
+     * 向量检索
+     */
+    public List<MilvusRetrievalResult> search(String question, String collectionName, Integer TopK){
+        //1.将问题转换成向量
+        Response<Embedding> embed = embeddingModel.embed(question);
+        List<Float> vector = embed.content().vectorAsList();
+        FloatVec floatVec = new FloatVec(vector);
+        List<BaseVector> list = Arrays.asList(floatVec);
+
+        // 2.构造SearchReq
+        SearchReq searchReq = SearchReq
+                                .builder()
+                                .collectionName(collectionName)
+                                .annsField("chunk_text_vector")
+                                .outputFields(Arrays.asList("chunk_text_varchar","metadata"))
+                                .data(list)
+                                .limit(TopK)
+                                .consistencyLevel(ConsistencyLevel.EVENTUALLY)
+                                .build();
+
+        SearchResp search = milvusClientV2.search(searchReq);
+        List<List<SearchResp.SearchResult>> searchResults = search.getSearchResults();
+
+        if (searchResults.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        // 4. 解析结果（无任何框架坑）
+        List<MilvusRetrievalResult> resultList = new ArrayList<>();
+        for (SearchResp.SearchResult searchResult : searchResults.get(0)) {
+            Map<String, Object> entity = searchResult.getEntity();
+
+            long id = ((Number)searchResult.getId()).longValue();
+            // 主键
+
+            // 对应Chunk文本
+            String text = (String) entity.get("chunk_text_varchar");
+            // 相似度分数
+            Float score = searchResult.getScore();
+            // 元数据
+            String metadataString = entity.get("metadata").toString();
+            Map<String, Object> metadataMap;
+            // 处理 null / 空字符串，避免转换报错
+            if (metadataString == null || metadataString.isBlank()) {
+                metadataMap = new HashMap<>();
+            } else {
+                // JSON 字符串 → Map<String,Object>
+                metadataMap = new Gson().fromJson(metadataString, Map.class);
+            }
+
+            MilvusRetrievalResult retrievalResult = MilvusRetrievalResult
+                    .builder()
+                    .chunkId(id)
+                    .text(text)
+                    .metadata(metadataMap)
+                    .score(score)
+                    .build();
+            resultList.add(retrievalResult);
+        }
+        return resultList;
     }
 
 
